056228fc5e3660fbe24aa0c6078218f066112325
cre8ture <bakukai@gmail.com>
2024-04-29 10:03:18 -0400

    new

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: 'brush and poem',
+    description: 'Create simple poems inspired by brush painters',
+    author: 'Kai Kleinbard',
+  },
+  plugins: [
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `articles`,
+        path: `${__dirname}/src/articles/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    // other gatsby plugins
+  ],
+};
